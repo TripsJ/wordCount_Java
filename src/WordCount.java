5bf644f6177f522a3aef1504e5b2e957105a4b62
Jérôme Trips <jerome.trips@tutanota.com>
2023-01-15 13:17:38 +0100

    added stream close

--- src/WordCount.java
+++ src/WordCount.java
@@ -2,6 +2,7 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class WordCount {
@@ -9,12 +10,24 @@ public class WordCount {
         File file = new File("Lorem.txt");
         try {
             Scanner scanner = new Scanner(new FileInputStream(file));
+            HashMap<String, Integer> map = new HashMap<>();
             int count =0;
             while(scanner.hasNext()){
-                scanner.next();
                 count++;
+                String word = scanner.next().toLowerCase();
+                if (map.containsKey(word)){
+                    map.put(word,map.get(word) +1);
+                }
+                else{
+                    map.put(word,1);
+                }
+
+
             }
+            scanner.close();
             System.out.println(count);
+            System.out.println(map);
+
 
         } catch (FileNotFoundException e) {
             System.out.println("File not Found");
